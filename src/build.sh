67e5e7aaa84949fc1e3d09b27f88e2f066c270be
Xie Zhenye <xiezhenye@gmail.com>
2015-08-21 20:46:04 +0800

    remove unused binding

--- src/build.sh
+++ src/build.sh
@@ -3,9 +3,8 @@ BINLOG_SRC="/root/mysql-binlog-events-1.0.0-src"
 
 LIB_MYSQL="${MYSQL_SRC}/libmysql"
 LIB_BINLOG="${BINLOG_SRC}/libbinlogevents"
-BINDINGS="${BINLOG_SRC}/bindings"
 
-INCLUDES="-I${LIB_BINLOG}/include -I${LIB_BINLOG}/export -I${BINDINGS}/include"
+INCLUDES="-I${LIB_BINLOG}/include -I${LIB_BINLOG}/export" 
 
 g++ ${INCLUDES} binlog_undo.cc -c -std=c++11 -o binlog_undo.o
 g++ ${INCLUDES} main.cc -c -o main.o
